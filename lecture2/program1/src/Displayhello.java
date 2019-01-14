4198ba653e8544cfcbb79d4ccea871137cf3a0ff
Ananta <anantakadyan@gmail.com>
2019-01-14 21:54:38 +0530

    complied lecture 2 files in a folder

---
+++ lecture2/program1/src/Displayhello.java
@@ -0,0 +1,11 @@
+class Displayhello{
+
+	public static void main (String[]args){
+		//printing a line with multiple statements
+		/*it is a 
+		multiline comment*/
+
+		System.out.println("Hello ");
+		System.out.println("Hello World!");
+	}
+}
